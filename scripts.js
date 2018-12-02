07708c2b17c7ec3db2e11ab5e9c254590a64b4ad
Jan Schlorf <jan.schlorf@we-are-infinity.com>
2018-12-02 19:39:06 +0100

    Added counter per beverage; Added beer and mug

--- scripts.js
+++ scripts.js
@@ -1,15 +1,50 @@
-var beverages = []
+var availableBeverages = [{name: 'Apfel-Amaretto Punsch', price: 2.80, labelId: 'appleCounterLabel'},
+                          {name: 'Beerenpunsch', price: 2.80, labelId: 'berryCounterLabel'},
+                          {name: 'Glühwein', price: 2.80, labelId: 'wineCounterLabel'},
+                          {name: 'Kinderpunsch', price: 2.80, labelId: 'kidsCounterLabel'},
+                          {name: 'Bier', price: 2.50, labelId: 'beerCounterLabel'},
+                          {name: 'Häferl', price: 2.50, labelId: 'cupCounterLabel'}]
+var selectedBeverages = [];
 
 function add(beverage) {
-    beverages.push(beverage)
+    var obj = availableBeverages.find(o => o.name === beverage.name);
     
-    const sum = beverages.reduce(function(result,item) {
+    if (isRealValue(obj)) {
+        incrementValueFor(document.getElementById(obj.labelId));
+        selectedBeverages.push(beverage);
+    } else {
+        alert('Not found: ' + beverage.name + "; " + availableBeverages.length)
+    }
+    
+    updateSum();
+}
+
+function incrementValueFor(label) {
+    var currentCount = parseInt(label.innerHTML);
+    label.innerHTML = currentCount+1;
+}
+
+function updateSum() {
+    const sum = selectedBeverages
+    .reduce(function(result,item) {
         return result + item.price;
     }, 0);
     
     document.getElementById('result').innerHTML = sum.toFixed(2) + '€'
 }
 
+function isRealValue(obj) {
+    return obj && obj !== 'null' && obj !== 'undefined';
+}
+
+function resetCounters() {
+    for (var i = 0; i < availableBeverages.length; i++) {
+        var item = availableBeverages[i]
+        (document.getElementById(item.labelId)).innerHTML = 0
+    }
+}
+
+// Disable double tap on mobile
 document.addEventListener('touchmove', function(event) {
     event = event.originalEvent || event;
     if(event.scale > 1) {
